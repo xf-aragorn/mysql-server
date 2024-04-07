f1741a8aed5717f3f369cd4a4871695fe511bb34
J0234 <xufeng@4px.com>
2024-04-07 08:48:34 +0800

    本地构建

---
+++ include/installed_headers.cc
@@ -0,0 +1,15 @@
+#include "errmsg.h"
+#include "my_command.h"
+#include "my_compress.h"
+#include "my_list.h"
+#include "mysql.h"
+#include "mysql_com.h"
+#include "mysql_time.h"
+#include "/mnt/e/src/mysql-server/include/mysql_version.h"
+#include "/mnt/e/src/mysql-server/include/mysqld_error.h"
+#include "field_types.h"
+#include "mysql/client_plugin.h"
+#include "mysql/plugin_auth_common.h"
+#include "mysql/udf_registration_types.h"
+int main(int, char **) { return 0; }
+
